b2417cae299c097dc6dec8651f0c75b122756009
Ubuntu <ubuntu@ip-172-31-45-116.ap-south-1.compute.internal>
2024-10-03 04:17:29 +0000

    Added files

---
+++ Day01/hello.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This is script for shell tutorial
+
+echo "Hello Guys"
+
+echo "i am learning Shell Scripting "
---
+++ Day02/demo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+<< comment
+you can write anything here.
+comment
+
+name="Jetha"
+
+echo "Name is $name, and Date is $(date)"
+
+read username
+
+
+echo "You entered $username"
+
+echo "The character in $0 are: $1 $2"
